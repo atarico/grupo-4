9f31760d1d47b21a4d264be3b39a7728c828eab2
jordu85 <jorgelinadusso@gmail.com>
2024-10-21 14:37:21 -0300

    creacion enums y clase abstracta proyecto

--- TechInnovate/Utils/Enums.cs
+++ TechInnovate/Utils/Enums.cs
@@ -1,6 +1,25 @@
 ﻿namespace TechInnovate
 {
+    enum Estado
+    {
+        PLANIFICACION,
+        EN_DESARROLLO,
+        EN_PRUEBAS,
+        COMPLETADO,
+        CANCELADO
+    }
 
+    enum Tipo
+    {
+        DESARROLLO_WEB,
+        DESARROLLO_MOVIL
+    }
 
+    enum Tecnologia
+    {
+        ANGULAR, 
+        REACT,
+        VUE_JS
+    }
 
 }
