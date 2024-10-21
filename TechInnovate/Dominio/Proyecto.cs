8ab4f69d453d39b144c891e8660469d82c1d8bef
jordu85 <jorgelinadusso@gmail.com>
2024-10-21 14:51:28 -0300

    clase abstracta proyecto

--- TechInnovate/Dominio/Proyecto.cs
+++ TechInnovate/Dominio/Proyecto.cs
@@ -3,5 +3,12 @@
     abstract public class Proyecto
     {
         private string _nombreProyecto;
+        private int _cantidadDesarrolladores;
+        private DateTime _fechaInicio;
+        private Estado _estadoProyecto;
+        private Tipo _tipoDesarrollo;
+
+        public abstract void DuracionDelProyecto();
+        
     }
 }
